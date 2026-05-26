92b3068c099728385a4063046fbd151a5b6f4fed
100073Y <rebecasayuriogawa@alunos.utfpr.edu.br>
2026-05-26 08:57:01 -0300

    Create stack.cpp
    aula de stack ldp

---
+++ stack.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num1, num2;
+cout << "Digite dois números inteiros: ";
+cin >> num1 >> num2;
+cout << "Soma: " << num1 + num2 << endl;
+return 0;
+}
